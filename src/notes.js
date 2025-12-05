ed4fd2e5630d3e3934332a69972a2c349c1fbfe0
Francho76 <francis.adolfo.gomez@gmail.com>
2025-12-05 15:50:24 -0500

    Primer commit del proyecto

---
+++ src/notes.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/api/notes'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const cors = require('cors')
+
+app.use(cors())
+
+// ...
+
+export default { getAll, create, update }
